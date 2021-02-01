911c3c9f4177b2d95b0349325a26d3f20a0b1268
Zane D. R <zane.reick@unknownts.tk>
2021-02-01 11:15:51 -0600

    Trying to make working with the OIDs easier

--- snmp.py
+++ snmp.py
@@ -1,6 +1,7 @@
 import configparser, sys
 from pysnmp import hlapi
-from quicksnmp import get
+from quicksnmp import get, get_bulk_auto
+from snmp_oids import *
 
 # Read config
 try:
@@ -11,5 +12,24 @@ except Exception as exp:
     sys.exit()
 
 community = config.get('General', 'community')
+host = config.get('General', 'router_ip')
+'''
+#megabytes=bytes/1048576
+its = get_bulk_auto(host, ['1.3.6.1.2.1.2.2.1.2 ', '1.3.6.1.2.1.31.1.1.1.18', '1.3.6.1.2.1.2.2.1.10', '1.3.6.1.2.1.2.2.1.16'], hlapi.CommunityData(community), '1.3.6.1.2.1.2.1.0')
+for it in its:
+    for k, v in it.items():
+        if '1.3.6.1.2.1.2.2.1.10' in k or '1.3.6.1.2.1.2.2.1.16' in k:
+            v = v/1073741824
+        if '1.3.6.1.2.1.31.1.1.1.18' in k:
+            pass
+        else:
+            print("{0}={1}".format(k, v))
+    print('')
+'''
+#                  Bytes in                      Bytes out              Get interface 1 info     Get interface 1 info
+oidsToLookup = [hostname_oid, min1_load_oid, total_mem_oid, total_swap_oid, uptime_oid]
 
-print(get('10.0.0.1', ['1.3.6.1.2.1.1.5.0'], hlapi.CommunityData(community)))
+for oidStr in oidsToLookup:
+    oid = [oidStr]
+#    oid[1] = oidStr
+    print(get(host, oid, hlapi.CommunityData(community)))
---
+++ snmp_oids.py
@@ -0,0 +1,19 @@
+'''
+Zane Reick 2021
+This is centered around PfSense, and as such some OIDs might not work on some systems
+'''
+
+hostname_oid = '1.3.6.1.2.1.1.5.0'
+bytes_in_oid = '1.3.6.1.2.1.2.2.1.10.1'
+bytes_out_oid = '1.3.6.1.2.1.2.2.1.16.1'
+interface_oid = '1.3.6.1.2.1.2.2.1.2'
+interface_oid_2 = '1.3.6.1.2.1.31.1.1.1.18.1'
+min1_load_oid = '1.3.6.1.4.1.2021.10.1.3.1'
+min5_load_oid = '1.3.6.1.4.1.2021.10.1.3.2'
+min15_load_oid = '1.3.6.1.4.1.2021.10.1.3.3'
+total_swap_oid = '1.3.6.1.4.1.2021.4.3.0'
+free_swap_oid = '1.3.6.1.4.1.2021.4.4.0'
+total_mem_oid = '1.3.6.1.4.1.2021.4.5.0'
+used_mem_oid = '1.3.6.1.4.1.2021.4.6.0'
+free_mem_oid = '1.3.6.1.4.1.2021.4.11.0'
+uptime_oid = '1.3.6.1.2.1.1.3.0'
