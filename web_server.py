1c5e6be7ea7d9e4a4b3c5538defc73febdb71ae0
Zane Reick <zane.reick@unknownts.tk>
2021-02-03 20:07:20 -0600

    Started on the web server portion

---
+++ web_server.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOST = "0.0.0.0"
+PORT = 8080
+
+class webServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Test</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    ws = HTTPServer((HOST, PORT), webServer)
+    print("Server started http://%s:%s" % (HOST, PORT))
+
+    try:
+        ws.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    ws.server_close()
+    print("Server stopped.")
