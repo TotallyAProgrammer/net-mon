9988ab51923802ea442c2e98821fad0f89863a7f
Zane Reick <zane.reick@unknownts.tk>
2021-02-04 16:10:40 -0600

    Making magic happen. Magic is fake, this is code

--- network_checks.py
+++ network_checks.py
@@ -1,6 +1,11 @@
 import socket
+import json
 
 def tcpCheck(ip, port, timeout):
+    '''
+    See if host is up
+    Returns true or false
+    '''
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(timeout)
     try:
@@ -12,3 +17,27 @@ def tcpCheck(ip, port, timeout):
     finally:
         s.close()
 
+def updateData(dataStorage, dataToStore):
+    '''
+    Update the data that is being stored
+    Input data and where to store it
+    '''
+    data = {}
+    jsonData = json.dumps(data)
+    try:
+        with open('network_log.json', 'a') as output:
+            output.write(jsonData + "\n")
+            return True
+    except Exception as exp:
+        print("Exception: " + str(exp))
+        return False
+
+def readData():
+    pass
+
+def checkHost(checkType):
+    checkType = str(checkType).lower()
+    if (checkType == "icmp"):
+        pass
+    elif (checkType == "tcp"):
+        pass
