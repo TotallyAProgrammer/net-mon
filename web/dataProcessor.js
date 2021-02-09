7d751ac9a9dab43d71fedfe13d7e7a3f95ffe74b
Zane Reick <zane.reick@unknownts.tk>
2021-02-09 00:00:48 -0600

    Cont'd work on the web interface. Script is a decent state. Can read the file

--- web/dataProcessor.js
+++ web/dataProcessor.js
@@ -1,1 +1,26 @@
 var dataContainer = document.getElementById("networkData");
+
+function readTextFile(file, alertUser) {
+    var allText = "";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                allText = rawFile.responseText;
+                if (alertUser === 1) {
+                    alert(allText);
+                }
+                
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
+}
+
+function parseMassJSON(fileName) {
+    console.log(readTextFile(fileName, 0))
+}
+
+parseMassJSON("/network_log.json");
